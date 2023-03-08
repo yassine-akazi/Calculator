8a05377cf3f1299db203995e8b9efefc2a405676
Yassine Akazi <96472822+yassine-akazi@users.noreply.github.com>
2023-03-09 00:07:16 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,12 +1,19 @@
-from tkinter import* 
+# import necessary modules from tkinter library
+from tkinter import*
 from tkinter import ttk 
+
+# create a new instance of Tk and set window title
 root = Tk()
 root.title("calculator")
+
+# prevent window from being resized
 root.resizable(0,0)
+
+# define function to handle button clicks
 def insert(text):
-    if text != "=":
+    if text != "=":  # if not "=" button, add text to En
         En.insert(END , text)
-    else:
+    else:  # if "=" button, evaluate En contents and display result or error
         var = En.get()
         try:
             operation = eval(var)
@@ -16,13 +23,16 @@ def insert(text):
             En.delete(0, "end")
             E.insert(0 , "math error")
 
+# define function to create buttons
 def button(text , clmn , rw ):
+    # create a new ttk button with specified text and command, and add to grid at specified column and row
     ttk.Button(root , text = text , command = lambda:insert(text)).grid(column = clmn , row = rw , ipady=6 , ipadx=1) 
-    
+
+# create a new Entry widget and add to grid
 En = ttk.Entry(root , font="arial 15")
 En.grid(column= 0 , row = 0 , columnspan= 5 , ipadx=4 , ipady=8 )
 
-
+# create buttons for numbers and operators, using button function to add to grid
 button("1", 0, 1)
 button("2", 1, 1)
 button("3", 2, 1)
@@ -44,4 +54,5 @@ button(".", 1, 4)
 button("=", 2, 4)
 button("0", 0, 4)
 
+# start main event loop
 root.mainloop()
